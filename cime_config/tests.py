0e430372d6c09e6955a553359660abd1058ed089
daliwang <wangd@ornl.gov>
2024-01-18 17:24:35 -0500

    Changes for docker configuration and adding a test specific for Ameriflux CPL_BYPASS runs

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -94,6 +94,8 @@ _TESTS = {
             "ERS.f09_f09.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"
+            "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
+            "SMS_Ln2.hcru_hcru.ICB1850CNRDCTCBC.elm-ameriflux_cplbypass"
             )
         },
 
